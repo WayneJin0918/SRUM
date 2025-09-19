774cce278319d328498a0c7af52207be8a582139
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2025-09-19 19:44:35 +0800

    Update vlm.sh

--- srum_data_infer/vlm.sh
+++ srum_data_infer/vlm.sh
@@ -8,4 +8,5 @@ python srum_data_infer/vlm_multi_gpu.py \
 
 python srum_data_infer/vlm_format_output.py \
     --input_jsonl regional_rewards/images_comp_base_7b_50_step_think_regional_rewards.jsonl \
-    --output_jsonl images_comp/images_comp_base_7b_50_step_think_regional_rewards.jsonl
+    --output_jsonl images_comp/images_comp_base_7b_50_step_think_regional_rewards.jsonl \
+    --original_jsonl images_comp/bagel_base_7b_50_step_think/output.jsonl
